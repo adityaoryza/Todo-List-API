a4ea0e9df1ce38310951de93c3573fa52ce8e5b8
adityaoryza <oryza429@gmail.com>
2023-05-11 03:04:58 +0700

    install dotenv package

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+require("dotenv").config();
 
 // import routes
 const todos = require("./routes/todoRoutes");
